24ad8cf17841828fb1ced45e219b911c7dc799df
metumetehan <metumetehan@gmail.com>
2024-02-12 10:39:49 +0300

    Initial commit

---
+++ src/main/java/dev/metehan/contentcalendar/Application.java
@@ -0,0 +1,17 @@
+package dev.metehan.contentcalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.sql.SQLOutput;
+import java.util.Arrays;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/main/java/dev/metehan/contentcalendar/Message.java
@@ -0,0 +1,10 @@
+package dev.metehan.contentcalendar;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Message {
+    public String getMessage() {
+        return "Hellooo";
+    }
+}
